9d3449b3f98cd01998060066c8d26117d1a38389
Himanshu <ht5620085@gmail.com>
2026-02-10 13:27:22 +0530

    bug fix: changing text from black to white

--- frontend/script.js
+++ frontend/script.js
@@ -27,7 +27,7 @@ async function fetchTodos() {
       "flex justify-between items-center px-3 py-2 border-b border-slate-700";
 
     li.innerHTML = `
-      <span class="${todo.completed ? "line-through text-gray-400" : ""}">
+      <span class="text-white ${todo.completed ? "line-through text-gray-400" : ""}">
         ${todo.text}
       </span>
       <div class="flex gap-2">
